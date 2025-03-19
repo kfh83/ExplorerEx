65952204f35a771172339cfb5ae8b0d4c938285c
Olive <oliveistyping@gmail.com>
2025-03-18 20:37:17 -0600

    [Tray] Simplify _SetBandSiteTheme else condition
    Thx aubrey

--- src/Tray.cpp
+++ src/Tray.cpp
@@ -4460,9 +4460,7 @@ void CTray::_SetBandSiteTheme()
     }
     else
     {
-        pszClassList = L"TaskBarVertComposited";
-        if (!fComposited)
-            pszClassList = L"TaskBarVert";
+        pszClassList = fComposited ? L"TaskBarVertComposited" : L"TaskBarVert";
     }
     BandSite_SetWindowTheme(_bandSite, (LPWSTR)pszClassList);
 }
