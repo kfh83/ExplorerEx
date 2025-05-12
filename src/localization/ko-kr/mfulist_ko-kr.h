e14261f4a0dab1a31d89646ba713a559df49bc4c
__haeppy (ป๊อปเป้อ) <119104342+Longhorn004@users.noreply.github.com>
2025-05-12 09:11:29 +0900

    Korean translation support for ExplorerEx (#16)
    * Create StartPane.ko-kr.rc
    
    * Add files via upload
    
    * Add files via upload
    
    * Update Explorer.vcxproj
    
    * Adding the ko-kr to compiler
    
    * Update Explorer.vcxproj
    
    * fixing some minor mistakes in ko-kr rc
    
    * Update Explorer.ko-kr.rc
    
    * Update dlg.ko-kr.rc
    
    * Update Explorer.vcxproj
    
    * Update dlg.ko-kr.rc
    
    * small updates

---
+++ src/localization/ko-kr/mfulist_ko-kr.h
@@ -0,0 +1,116 @@
+/*
+ *  mfulist.h - The default MFU lists
+ *
+ *  The MFU lists need to be replicated for MUI purposes, so we centralize
+ *  them here.
+ *
+ */
+
+#define MFU_SETDEFAULTS "%ALLUSERSPROFILE%\\시작 메뉴\\기본 프로그램 설정.lnk"
+
+//
+//  32-bit Client for all user types
+//
+#define MFU_PRO32ALL_00 "%USERPROFILE%\\시작 메뉴\\프로그램\\Internet Explorer.lnk"
+#define MFU_PRO32ALL_01 "%ALLUSERSPROFILE%\\시작 메뉴\\프로그램\\보조프로그램\\미디어 센터\\미디어 센터.lnk"
+#define MFU_PRO32ALL_02 "%ALLUSERSPROFILE%\\시작 메뉴\\프로그램\\Windows 필기장.lnk"
+#define MFU_PRO32ALL_03 "%ALLUSERSPROFILE%\\시작 메뉴\\기본 프로그램 설정.lnk"
+#define MFU_PRO32ALL_04 "%ALLUSERSPROFILE%\\시작 메뉴\\프로그램\\Tablet PC 사용 안내.lnk"
+#define MFU_PRO32ALL_05 "%ALLUSERSPROFILE%\\시작 메뉴\\프로그램\\MSN으로 인터넷 연결.lnk"
+#define MFU_PRO32ALL_06 "%ALLUSERSPROFILE%\\시작 메뉴\\프로그램\\MSN Explorer.lnk"
+#define MFU_PRO32ALL_07 "%USERPROFILE%\\시작 메뉴\\프로그램\\Windows Media Player.lnk"
+#define MFU_PRO32ALL_08 "%ALLUSERSPROFILE%\\시작 메뉴\\프로그램\\Windows Messenger.lnk"
+#define MFU_PRO32ALL_09 "%USERPROFILE%\\시작 메뉴\\프로그램\\보조프로그램\\Windows XP 둘러보기.lnk"
+#define MFU_PRO32ALL_10 "%ALLUSERSPROFILE%\\시작 메뉴\\프로그램\\보조프로그램\\Windows Movie Maker.lnk"
+#define MFU_PRO32ALL_11 "%ALLUSERSPROFILE%\\시작 메뉴\\프로그램\\보조프로그램\\시스템 도구\\파일 및 설정 전송 마법사.lnk"
+#define MFU_PRO32ALL_12 ""
+#define MFU_PRO32ALL_13 ""
+#define MFU_PRO32ALL_14 ""
+#define MFU_PRO32ALL_15 ""
+
+//
+//  64-bit Client for all user types
+//
+#define MFU_PRO64ALL_00 "%ALLUSERSPROFILE%\\시작 메뉴\\프로그램\\보조프로그램\\미디어 센터\\미디어 센터.lnk"
+#define MFU_PRO64ALL_01 "%ALLUSERSPROFILE%\\시작 메뉴\\프로그램\\Windows 필기장.lnk"
+#define MFU_PRO64ALL_02 "%ALLUSERSPROFILE%\\시작 메뉴\\기본 프로그램 설정.lnk"
+#define MFU_PRO64ALL_03 "%ALLUSERSPROFILE%\\시작 메뉴\\프로그램\\Tablet PC 사용 안내.lnk"
+#define MFU_PRO64ALL_04 "%ALLUSERSPROFILE%\\시작 메뉴\\프로그램\\MSN으로 인터넷 연결.lnk	"
+#define MFU_PRO64ALL_05 "%ALLUSERSPROFILE%\\시작 메뉴\\프로그램\\MSN Explorer.lnk"
+#define MFU_PRO64ALL_06 "%USERPROFILE%\\시작 메뉴\\프로그램\\Windows Media Player.lnk"
+#define MFU_PRO64ALL_07 "%ALLUSERSPROFILE%\\시작 메뉴\\프로그램\\Windows Messenger.lnk"
+#define MFU_PRO64ALL_08 "%USERPROFILE%\\시작 메뉴\\프로그램\\보조프로그램\\Windows XP 둘러보기.lnk"
+#define MFU_PRO64ALL_09 "%ALLUSERSPROFILE%\\시작 메뉴\\프로그램\\보조프로그램\\Windows Movie Maker.lnk"
+#define MFU_PRO64ALL_10 "%ALLUSERSPROFILE%\\시작 메뉴\\프로그램\\보조프로그램\\시스템 도구\\파일 및 설정 전송 마법사.lnk"
+#define MFU_PRO64ALL_11 ""
+#define MFU_PRO64ALL_12 ""
+#define MFU_PRO64ALL_13 ""
+#define MFU_PRO64ALL_14 ""
+#define MFU_PRO64ALL_15 ""
+
+//
+//  32-bit Server for administrators
+//
+#define MFU_SRV32ADM_00 "%USERPROFILE%\\시작 메뉴\\프로그램\\보조프로그램\\명령 프롬프트.lnk"
+#define MFU_SRV32ADM_01 "%USERPROFILE%\\시작 메뉴\\프로그램\\보조프로그램\\메모장.lnk"
+#define MFU_SRV32ADM_02 ""
+#define MFU_SRV32ADM_03 ""
+#define MFU_SRV32ADM_04 ""
+#define MFU_SRV32ADM_05 ""
+#define MFU_SRV32ADM_06 ""
+#define MFU_SRV32ADM_07 ""
+#define MFU_SRV32ADM_08 ""
+#define MFU_SRV32ADM_09 ""
+#define MFU_SRV32ADM_10 ""
+#define MFU_SRV32ADM_11 ""
+#define MFU_SRV32ADM_12 ""
+#define MFU_SRV32ADM_13 ""
+#define MFU_SRV32ADM_14 ""
+#define MFU_SRV32ADM_15 ""
+
+//
+//  64-bit Server for administrators
+//
+#define MFU_SRV64ADM_00 "%USERPROFILE%\\시작 메뉴\\프로그램\\보조프로그램\\명령 프롬프트.lnk"
+#define MFU_SRV64ADM_01 "%USERPROFILE%\\시작 메뉴\\프로그램\\보조프로그램\\메모장.lnk"
+#define MFU_SRV64ADM_02 ""
+#define MFU_SRV64ADM_03 ""
+#define MFU_SRV64ADM_04 ""
+#define MFU_SRV64ADM_05 ""
+#define MFU_SRV64ADM_06 ""
+#define MFU_SRV64ADM_07 ""
+#define MFU_SRV64ADM_08 ""
+#define MFU_SRV64ADM_09 ""
+#define MFU_SRV64ADM_10 ""
+#define MFU_SRV64ADM_11 ""
+#define MFU_SRV64ADM_12 ""
+#define MFU_SRV64ADM_13 ""
+#define MFU_SRV64ADM_14 ""
+#define MFU_SRV64ADM_15 ""
+
+//
+//  Macros
+//
+#define MFU_ENUM(fn, type)             \
+    fn(type##_00, MFU_##type##_00)     \
+    fn(type##_01, MFU_##type##_01)     \
+    fn(type##_02, MFU_##type##_02)     \
+    fn(type##_03, MFU_##type##_03)     \
+    fn(type##_04, MFU_##type##_04)     \
+    fn(type##_05, MFU_##type##_05)     \
+    fn(type##_06, MFU_##type##_06)     \
+    fn(type##_07, MFU_##type##_07)     \
+    fn(type##_08, MFU_##type##_08)     \
+    fn(type##_09, MFU_##type##_09)     \
+    fn(type##_10, MFU_##type##_10)     \
+    fn(type##_11, MFU_##type##_11)     \
+    fn(type##_12, MFU_##type##_12)     \
+    fn(type##_13, MFU_##type##_13)     \
+    fn(type##_14, MFU_##type##_14)     \
+    fn(type##_15, MFU_##type##_15)     \
+
+#define MFUENUM_CSTR(nm, val)   TEXT(val),
+#define MFUENUM_RCSTR(nm, val)  IDS_MFU_##nm val
+
+#define MFU_ENUMRC(type)        MFU_ENUM(MFUENUM_RCSTR, type)
+#define MFU_ENUMC(type)         MFU_ENUM(MFUENUM_CSTR,  type)
