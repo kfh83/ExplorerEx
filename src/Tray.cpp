5e32ebad1799d2e49990b273630c0c431d039866
tetawaves <130876208+tetawaves@users.noreply.github.com>
2025-10-14 20:37:28 +0200

    Set property "TaskbandHWND" to tray window
    Fixes #53

--- src/Tray.cpp
+++ src/Tray.cpp
@@ -1780,6 +1780,8 @@ void CTray::_InitBandsite()
 
     BandSite_FindBand(_ptbs, CLSID_TaskBand, IID_PPV_ARG(IDeskBand, &_pdbTasks), NULL, NULL);
     IUnknown_GetWindow(_pdbTasks, &_hwndTasks);
+    // ITaskbarList interface uses this property to get the tasks window
+    SetProp(_hwnd, L"TaskbandHWND", _hwndTasks);
 
     // Now that bandsite is ready, set the correct size
     VerifySize(FALSE, TRUE);
@@ -4172,6 +4174,7 @@ LRESULT CTray::_HandleDestroy()
 
     ATOMICRELEASE(_ptbs);
     ATOMICRELEASE(_pdbTasks);
+    RemoveProp(_hwnd, L"TaskbandHWND");
     _hwndTasks = NULL;
 
     if (_hwndTrayTips)
