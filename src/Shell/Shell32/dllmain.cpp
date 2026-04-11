cd1ab22df1e30408e91dcfef0e87bc0387b0697b
AllieTheFox <AllieIsTyping@gmail.com>
2026-04-11 16:48:03 -0600

    feat: Begin initial work on DllGetClassFactory

--- src/Shell/Shell32/dllmain.cpp
+++ src/Shell/Shell32/dllmain.cpp
@@ -1,5 +1,27 @@
 #include "pch.h"
 
+/*
+* This will eventually be the host for the following CLSIDs + Implementations.
+* CLSID_StartMenu                   - CStartMenu_CreateInstance
+* CLSID_PersonalStartMenu           - CPersonalStartMenu_CreateInstance
+* CLSID_StartMenuFolder             - CStartMenuFolder_CreateInstance
+* CLSID_ProgramsFolder              - CProgramsFolder_CreateInstance
+* CLSID_ProgramsFolderAndFastItems  - CProgramsFolderAndFastItems_CreateInstance
+*/
+
+STDAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, void** ppv)
+{
+    UNREFERENCED_PARAMETER(rclsid);
+    UNREFERENCED_PARAMETER(riid);
+
+    if (ppv != nullptr)
+    {
+        *ppv = nullptr;
+    }
+
+    return CLASS_E_CLASSNOTAVAILABLE;
+}
+
 STDAPI_(BOOL) DllMain(HANDLE hDll, DWORD dwReason, void* lpReserved)
 {
     switch (dwReason)
